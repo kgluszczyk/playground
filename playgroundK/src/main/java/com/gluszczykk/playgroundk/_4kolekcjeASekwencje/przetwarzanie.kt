461b6ade47bb1ae4eca0e58bbf98c328ed2e1fcf
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-23 04:01:22 +0100

    Initial commit

--- playgroundK/src/main/java/com/gluszczykk/playgroundk/_4kolekcjeASekwencje/przetwarzanie.kt
+++ playgroundK/src/main/java/com/gluszczykk/playgroundk/_4kolekcjeASekwencje/przetwarzanie.kt
@@ -5,7 +5,7 @@ data class Osoba(val imie: String, val wiek: Int)
 /** Zadanie 3
  * Kolekcje vs Sekwencje
  * Zaimplementuj funkcję, która zwróci pojedyńcze występnienia imion pełnolatków
- *
+ * Którą formę ewaluacji wybierzesz?
  */
 
 fun List<Osoba>.unikalnePelnoletnieImiona() = emptyList<String>()
